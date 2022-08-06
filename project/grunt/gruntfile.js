103869f862494c564c7b571d01989f73dbcef3c9
JeraldVictor <jeraldvictor123@yahoo.com>
2022-08-06 22:43:28 +0530

    Grunt Js Grunt Watch on file change [css & js]

--- project/grunt/gruntfile.js
+++ project/grunt/gruntfile.js
@@ -1,19 +1,34 @@
 module.exports = function(grunt){
-    grunt.loadNpmTasks('grunt-contrib-concat');
-
     grunt.initConfig({
         concat: {
           options: {
               separator: '\n',
               sourceMap:true,
+              banner:`/*Created on ${new Date()}*/\n`,
           },
-          dist:{
-            src:['../css/*.css'],
+          css:{
+            src:['../css/**/*.css'],
             dest:'../../App/css/style.css'
+          },
+          js:{
+            src:['../lib/jquery/jquery.min.js','../js/**/*.js'],
+            dest:'../../App/js/app.js'
           }
-        }
+        },
+        watch: {
+          css: {
+            files: ['../css/**/*.css'],
+            tasks: ['concat'],
+            options: {
+              spawn: false,
+            },
+          },
+        },
       });
    
+      grunt.loadNpmTasks('grunt-contrib-concat');
+      grunt.loadNpmTasks('grunt-contrib-watch');
+  
    
-    grunt.registerTask('default',['concat']);
+    grunt.registerTask('default',['concat','watch']);
 }

fdad8e7b8d361cb20d260c47af9c1831c27e9be5
JeraldVictor <jeraldvictor123@yahoo.com>
2022-08-06 23:09:54 +0530

    Grunt minify js and css

--- project/grunt/gruntfile.js
+++ project/grunt/gruntfile.js
@@ -1,34 +1,63 @@
-module.exports = function(grunt){
-    grunt.initConfig({
-        concat: {
-          options: {
-              separator: '\n',
-              sourceMap:true,
-              banner:`/*Created on ${new Date()}*/\n`,
-          },
-          css:{
-            src:['../css/**/*.css'],
-            dest:'../../App/css/style.css'
-          },
-          js:{
-            src:['../lib/jquery/jquery.min.js','../js/**/*.js'],
-            dest:'../../App/js/app.js'
-          }
+module.exports = function (grunt) {
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: "\n",
+        sourceMap: true,
+        banner: `/*Created on ${new Date()}*/\n`,
+      },
+      css: {
+        src: ["../css/**/*.css"],
+        dest: "../dist/css/style.css",
+      },
+      js: {
+        src: ["../lib/jquery/jquery.min.js", "../js/**/*.js"],
+        dest: "../dist/js/app.js",
+      },
+    },
+    cssmin: {
+      target: {
+        options: {
+          mergeIntoShorthands: false,
+          roundingPrecision: -1,
         },
-        watch: {
-          css: {
-            files: ['../css/**/*.css'],
-            tasks: ['concat'],
-            options: {
-              spawn: false,
-            },
-          },
+        files: {
+          "../../App/css/style.css": ["../dist/css/style.css"],
         },
-      });
-   
-      grunt.loadNpmTasks('grunt-contrib-concat');
-      grunt.loadNpmTasks('grunt-contrib-watch');
-  
-   
-    grunt.registerTask('default',['concat','watch']);
-}
+      },
+    },
+    uglify: {
+      target: {
+        options: {
+          sourceMap: true,
+        },
+        files: {
+          "../../App/js/app.js": ["../dist/js/app.js"],
+        },
+      },
+    },
+    watch: {
+      css: {
+        files: ["../css/**/*.css"],
+        tasks: ["concat:css", "cssmin"],
+        options: {
+          spawn: false,
+        },
+      },
+      js: {
+        files: ["../js/**/*.js"],
+        tasks: ["concat:js", "uglify"],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-concat"); // Concat Files plugin
+  grunt.loadNpmTasks("grunt-contrib-watch"); // Watch file save Plugin
+  grunt.loadNpmTasks("grunt-contrib-cssmin"); // Css min plugin
+  grunt.loadNpmTasks("grunt-contrib-uglify"); // Js min plugin
+
+  grunt.registerTask("default", ["concat", "cssmin", "uglify", "watch"]);
+};
